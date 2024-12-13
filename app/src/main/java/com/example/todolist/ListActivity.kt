e12e07d7e812fc3bd32447fb6e0e8b69cd01d3e3
tommyallen <thomasclansi228@gmail.com>
2024-12-13 17:43:46 +0700

    Create ListActivity for show active tasks (Main Activity)

---
+++ app/src/main/java/com/example/todolist/ListActivity.kt
@@ -0,0 +1,30 @@
+package com.example.todolist
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ListActivity : AppCompatActivity() {
+    private lateinit var taskViewModel: TaskViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        val adapter = TaskAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        taskViewModel.allTasks.observe(this, { tasks ->
+            tasks?.let { adapter.submitList(it) }
+        })
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+            startActivity(Intent(this, AddActivity::class.java))
+        }
+    }
+}
